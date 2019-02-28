32e02781f7302964318dc355e950bab54240f9c5
Danny Rangel <danielrangel.tech@gmail.com>
2019-02-28 01:42:09 -0800

    changed env stuff

--- src/youtube.js
+++ src/youtube.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const KEY = "AIzaSyDCSVsGoiQKb3-IAVnxABTjoYwbyiRnLTI";
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet,contentDetails,statistics",
-    key: KEY
+    key: process.env.REACT_APP_API_KEY
   }
 });
