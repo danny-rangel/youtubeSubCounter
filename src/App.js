037c794c07b6bf9f4099d7ab959e80e76955f339
Danny Rangel <danielrangel.tech@gmail.com>
2019-03-10 03:04:10 -0700

    input size change

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
 import React, { useState } from "react";
 
+import youtube from './youtube';
 import "./App.css";
 import useSubCount from "./useSubCount";
 
 
 const App = () => {
-  const [username, setUsername] = useState("Pewdiepie");
+  const [id, setID] = useState("UC-lHJZR3Gqxm24_Vd_AJ5Yw");
+  const [username, setUsername] = useState("");
+
+  const fetchID = async username => {
+    const response = await youtube.get("/search", {
+      params: {
+        part: 'snippet',
+        id,
+        q: username,
+        type: 'channel',
+        maxResults: 2
+      }
+    });
+
+    const { data } = response;
+    if (data.pageInfo.totalResults !== 0) {
+      setID(data.items[0].id.channelId);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchID(username);
+  }
 
   const onInputChange = event => {
     setUsername(event.target.value);
   };
-  
+
+
   return (
     <div className="App">
       <div className="header">
         Youtube Live Sub Counter
       </div>
       <div className="content">
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             className="usernameInput"
             type="text"
             onChange={onInputChange}
             value={username}
+            placeholder="Search by Channel or ID"
           />
         </form>
         <div className="info">
-          {useSubCount(username)}
+          {useSubCount(id)}
         </div>
       </div>
     </div>
--- src/youtube.js
+++ src/youtube.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
-    part: "snippet,contentDetails,statistics",
     key: process.env.REACT_APP_API_KEY
   }
 });
