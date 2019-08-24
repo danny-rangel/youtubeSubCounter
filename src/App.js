1449d6a37ed0beb1572775623d1c2e8c2afa0358
Danny Rangel <danielrangel.tech@gmail.com>
2019-08-23 21:26:18 -0700

    changed styling

--- src/App.js
+++ src/App.js
@@ -1,63 +1,56 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 import youtube from './youtube';
-import "./App.css";
-import useSubCount from "./useSubCount";
-
+import './App.css';
+import useSubCount from './useSubCount';
 
 const App = () => {
-  const [id, setID] = useState("UC-lHJZR3Gqxm24_Vd_AJ5Yw");
-  const [username, setUsername] = useState("");
+    const [id, setID] = useState('UC-lHJZR3Gqxm24_Vd_AJ5Yw');
+    const [username, setUsername] = useState('');
 
-  const fetchID = async username => {
-    const response = await youtube.get("/search", {
-      params: {
-        part: 'snippet',
-        id,
-        q: username,
-        type: 'channel',
-        maxResults: 2
-      }
-    });
+    const fetchID = async username => {
+        const response = await youtube.get('/search', {
+            params: {
+                part: 'snippet',
+                id,
+                q: username,
+                type: 'channel',
+                maxResults: 2
+            }
+        });
 
-    const { data } = response;
-    if (data.pageInfo.totalResults !== 0) {
-      setID(data.items[0].id.channelId);
-    }
-  };
+        const { data } = response;
+        if (data.pageInfo.totalResults !== 0) {
+            setID(data.items[0].id.channelId);
+        }
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetchID(username);
-  }
+    const handleSubmit = event => {
+        event.preventDefault();
+        fetchID(username);
+    };
 
-  const onInputChange = event => {
-    setUsername(event.target.value);
-  };
+    const onInputChange = event => {
+        setUsername(event.target.value);
+    };
 
-
-  return (
-    <div className="App">
-      <div className="header">
-        Youtube Live Sub Counter
-      </div>
-      <div className="content">
-        <form onSubmit={handleSubmit}>
-          <input
-            className="usernameInput"
-            type="text"
-            onChange={onInputChange}
-            value={username}
-            placeholder="Search by Channel or ID"
-          />
-        </form>
-        <div className="info">
-          {useSubCount(id)}
+    return (
+        <div className="App">
+            <div className="header">Youtube Live Sub Counter</div>
+            <div className="content">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        className="usernameInput"
+                        type="text"
+                        onChange={onInputChange}
+                        value={username}
+                        placeholder="Search by Channel or ID"
+                    />
+                </form>
+                <div className="info">{useSubCount(id)}</div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-}
-
+    );
+};
 
 export default App;
--- src/useSubCount.js
+++ src/useSubCount.js
@@ -1,62 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import numeral from 'numeral';
 
-import youtube from "./youtube";
-import useInterval from "./useInterval";
+import youtube from './youtube';
+import useInterval from './useInterval';
 
 const useSubCount = id => {
-  const [subCount, setSubCount] = useState(0);
-  const [difference, setDifference] = useState(0);
-  const [title, setTitle] = useState(null);
-  const [thumbnail, setThumbnail] = useState(null);
+    const [subCount, setSubCount] = useState(0);
+    const [difference, setDifference] = useState(0);
+    const [title, setTitle] = useState(null);
+    const [thumbnail, setThumbnail] = useState(null);
 
-  useInterval(() => {
-    fetchSubCount(id);
-  }, 1000);
+    useInterval(() => {
+        fetchSubCount(id);
+    }, 1000);
 
-  const fetchSubCount = async id => {
-    const response = await youtube.get("/channels", {
-      params: {
-        part: "snippet,contentDetails,statistics",
-        id
-      }
-    });
+    const fetchSubCount = async id => {
+        const response = await youtube.get('/channels', {
+            params: {
+                part: 'snippet,contentDetails,statistics',
+                id
+            }
+        });
 
-    if (response.data.pageInfo.totalResults === 0) {
-      setSubCount(0);
-      setTitle("");
-    } else {
-      setDifference(
-        response.data.items[0].statistics.subscriberCount - subCount
-      );
-      setSubCount(response.data.items[0].statistics.subscriberCount);
-      setTitle(response.data.items[0].snippet.title);
-      setThumbnail(response.data.items[0].snippet.thumbnails.medium.url)
-    }
-  };
+        if (response.data.pageInfo.totalResults === 0) {
+            setSubCount(0);
+            setTitle('');
+        } else {
+            setDifference(
+                response.data.items[0].statistics.subscriberCount - subCount
+            );
+            setSubCount(response.data.items[0].statistics.subscriberCount);
+            setTitle(response.data.items[0].snippet.title);
+            setThumbnail(response.data.items[0].snippet.thumbnails.medium.url);
+        }
+    };
 
-  useEffect(() => {
-    fetchSubCount(id);
-  }, []);
+    useEffect(() => {
+        fetchSubCount(id);
+    }, []);
 
-  return (
-    <div>
-      <div className="title">
-      {title ? `${title}` : null}
-      </div>
-      <div className="avatar">
-        <img className="thumbnail" src={thumbnail} alt={title} ></img>
-      </div>
-      <div className="subCount">
-        {title ? numeral(subCount).format('0,0'): null}
-      </div>
-      {title ? `subscribers`: null}
-      <div className="difference" style={{ color: difference < 0 ? `red` : `limegreen` }}>
-        {difference < 0 ? `${difference}` : `+${difference}`}
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <h3 className="title">{title ? `${title}` : null}</h3>
+            <div className="avatar">
+                <img className="thumbnail" src={thumbnail} alt={title}></img>
+            </div>
+            <h3 className="subCount">
+                {title ? numeral(subCount).format('0,0') : null}
+            </h3>
+            <h3 style={{ justifySelf: 'center' }}>
+                {title ? `subscribers` : null}
+            </h3>
+            <h3
+                className="difference"
+                style={{ color: difference < 0 ? `red` : `limegreen` }}
+            >
+                {difference < 0 ? `${difference}` : `+${difference}`}
+            </h3>
+        </>
+    );
 };
-
 
 export default useSubCount;
